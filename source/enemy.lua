c1db44061030b115be54b01a29b54f1551d02739
syun <2dgames.jp@gmail.com>
2026-06-07 18:34:46 +0900

    遅延発射を実装

--- source/enemy.lua
+++ source/enemy.lua
@@ -9,6 +9,24 @@ import "CoreLibs/object"
 import "actor"
 import "game_context"
 
+-- 遅延弾発射情報.
+class ("DelayedBatteryInfo").extends()
+function DelayedBatteryInfo:init(angle, speed, delay, ax, ay)
+	self.angle = angle
+	self.speed = speed
+	self.delay = delay
+	self.ax = ax or 0
+	self.ay = ay or 0
+end
+function DelayedBatteryInfo:elapsed()
+	self.delay -= 1
+	if self.delay <= 0 then
+		return true -- 発射可能.
+	end
+	return false
+end
+
+-- 敵クラスの定義.
 class("Enemy").extends(Actor)
 
 -- コンストラクタ.
@@ -17,16 +35,24 @@ function Enemy:init(x, y)
 	self.bullets = GameContext.getInstance().bulletManager -- 弾を管理するActorManagerへの参照.
 	self.hp = 3 -- 敵のHP.
 	self.timer = 0
+	self.batteries = {} -- 遅延弾発射の情報を格納するテーブル.
 	print("Enemy created at (" .. x .. ", " .. y .. ")")
 end
 
 -- 弾を撃つ.
-function Enemy:bullet(angle, speed, ax, ay)
+function Enemy:bullet(angle, speed, delay, ax, ay)
 	if self.bullets == nil then
 		return
 	end
+	if delay == nil then delay = 0 end
 	if ax == nil then ax = 0 end
 	if ay == nil then ay = 0 end
+
+	if delay > 0 then
+		-- 遅延発射なのでリストに追加するだけ.
+		self:_addBattery(angle, speed, delay, ax, ay)
+		return
+	end
 
 	local bullet = self.bullets:create(self.x, self.y)
 	bullet:setVelocity(angle, speed)
@@ -40,8 +66,12 @@ function Enemy:update()
 	self.timer += 1
 	if self.timer % 30 == 0 then
 		local aim = self:getAim()
-		self:nWayBullet(3, aim, 5, 30) -- 毎秒1発、プレイヤーに向かって弾を撃つ.
+		for i = 0, 5 do
+			local delay = i * 3
+			self:nWayBullet(3, aim, 5, 30, delay) -- 毎秒1発、プレイヤーに向かって弾を撃つ.
+		end
 	end
+	self:_updateBatteries() -- 遅延弾発射の更新.
 end
 
 -- 敵種別を設定.
@@ -71,14 +101,30 @@ function Enemy:getAim()
 end
 
 -- N-Wayショットを撃つ.
-function Enemy:nWayBullet(n, angle, speed, spread)
+function Enemy:nWayBullet(n, angle, speed, spread, delay)
 	if self.bullets == nil then
 		return
 	end
 	local d = spread / n
 	local a = angle - (d * 0.5 * (n - 1))
 	for i = 0, n - 1 do
-		self:bullet(a, speed)
+		self:bullet(a, speed, delay)
 		a += d
+	end
+end
+
+-- 遅延弾発射の情報を追加する.
+function Enemy:_addBattery(angle, speed, delay, ax, ay)
+	table.insert(self.batteries, DelayedBatteryInfo(angle, speed, delay, ax, ay))
+end
+
+-- 遅延弾発射の更新.
+function Enemy:_updateBatteries()
+	for i = #self.batteries, 1, -1 do
+		local battery = self.batteries[i]
+		if battery:elapsed() then
+			self:bullet(battery.angle, battery.speed, 0, battery.ax, battery.ay)
+			table.remove(self.batteries, i) -- 発射したらリストから削除.
+		end
 	end
 end
